2e6c04b44388d96c1ecb24db7b46d72d6ed7fb93
Kawaii-Yuki-Kun <kawaiiyukikun@hotmail.com>
2024-01-19 12:56:19 +0200

    fixed errors, maybe, idk

--- build/git_update.sh
+++ build/git_update.sh
@@ -1,13 +1,13 @@
-#!/in/bash
+#!/bin/bash
 
 VERSION=""
 
 # Get parameters
 while getopts v: flag
 do
-    case "${flag}" in
-        v) VERSION=${OPTARG};;
-    esac
+  case "${flag}" in
+    v) VERSION=${OPTARG};;
+  esac
 done
 
 # Get highest tag number, and add v0.1.0 if doesn't exist
@@ -16,52 +16,50 @@ CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
 if [[ $CURRENT_VERSION == '' ]]
 then
-    CURRENT_VERSION='v0.1.0'
+  CURRENT_VERSION='v0.1.0'
 fi
 echo "Current Version: $CURRENT_VERSION"
 
-# Replace . with space so can split into an array
+# replace . with space so can split into an array
 CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
 
-# Get number parts
+# get number parts
 VNUM1=${CURRENT_VERSION_PARTS[0]}
 VNUM2=${CURRENT_VERSION_PARTS[1]}
 VNUM3=${CURRENT_VERSION_PARTS[2]}
 
 if [[ $VERSION == 'major' ]]
 then
-    VNUM1=$((VNUM1+1))
-    VNUM2=0
-    VNUM3=0
+  VNUM1=v$((VNUM1+1))
 elif [[ $VERSION == 'minor' ]]
 then
-    VNUM2=$((VNUM2+1))
-    VNUM3=0
+  VNUM2=$((VNUM2+1))
 elif [[ $VERSION == 'patch' ]]
 then
-    VNUM3=$((VNUM3+1))
+  VNUM3=$((VNUM3+1))
 else
-    echo "No version type (https://semver.org) or incorrect type specified, try: -v [major, minor, patch]"
-    exit 1
+  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
+  exit 1
 fi
 
-# Create new tag
-NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
+# create new tag
+NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
 echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
-# Get current hash and see if it's already has a tag
+# get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
 NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
-# Only tag if no tag already
-if [[ -z "$NEEDS_TAG" ]]; then
-    echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
-    git tag $NEW_TAG
-    git push --tags
+# only tag if no tag already
+if [ -z "$NEEDS_TAG" ]; then
+  echo "Tagged with $NEW_TAG"
+  git tag $NEW_TAG
+  git push --tags
+  git push
 else
-    echo "Already a tag on this commit"
+  echo "Already a tag on this commit"
 fi
 
-echo ::set-output name=new_tag::$NEW_TAG
+echo ::set-output name=git-tag::$NEW_TAG
 
 exit 0
